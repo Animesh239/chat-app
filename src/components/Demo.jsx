187cabedcff03c73244aa7fe27f43d0c19d6676d
Animesh239 <animeshbarik239@gmail.com>
2023-10-14 15:58:44 +0530

    irl

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
-  
   const [allArticles, setAllArticles] = useState([]);
   const [copied, setCopied] = useState("");
 
